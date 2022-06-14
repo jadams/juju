4ca8fc4e0d124de979cc559fbb7afc9b20092ce1
John Adams <git@jada.ms>
2022-06-14 15:46:11 +0000

    add script

---
+++ openstack/scipts/quota.sh
@@ -0,0 +1,5 @@
+openstack quota set --instances 20 c85ccaab507441c99a950af6079f472c
+openstack quota set --volumes 20 c85ccaab507441c99a950af6079f472c
+openstack quota set --cores 40 c85ccaab507441c99a950af6079f472c
+openstack quota set --ram 96000 c85ccaab507441c99a950af6079f472c
+openstack quota set --secgroups 40 c85ccaab507441c99a950af6079f472c
