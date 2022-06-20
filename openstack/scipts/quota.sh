bef7788267c0df98515da442c829539d4ad425ac
John Adams <git@jada.ms>
2022-06-20 23:42:50 +0000

    add quota enhancing script

--- openstack/scipts/quota.sh
+++ openstack/scipts/quota.sh
@@ -1,5 +1,8 @@
-openstack quota set --instances 20 c85ccaab507441c99a950af6079f472c
-openstack quota set --volumes 20 c85ccaab507441c99a950af6079f472c
-openstack quota set --cores 40 c85ccaab507441c99a950af6079f472c
-openstack quota set --ram 96000 c85ccaab507441c99a950af6079f472c
-openstack quota set --secgroups 40 c85ccaab507441c99a950af6079f472c
+#!/bin/bash
+set -xeuo pipefail
+eval $(openstack project show --format shell --domain $OS_PROJECT_DOMAIN_NAME $OS_PROJECT_NAME)
+openstack quota set --instances 20 $id
+openstack quota set --volumes 20 $id
+openstack quota set --cores 40 $id
+openstack quota set --ram 96000 $id
+openstack quota set --secgroups 40 $id
